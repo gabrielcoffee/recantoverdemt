6c5951c603ae4a815216c62bf63ceb32cdafd050
Gabriel Fernandes <60583301+gabrielcoffee@users.noreply.github.com>
2024-02-09 09:08:32 -0400

    página inicial

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./dist/*.{html,js}",
+  ],
+  theme: {
+    fontFamily: {
+      'questrial': ['Questrial', 'sans'],
+      'nunito': ['Nunito', 'sans']
+    },
+    extend: {
+      colors: {
+        'mygreen': ['#3F5553'],
+        'mybege': ['#EFE7E6'],
+        'mygreenlight': ['#517A76'],
+        'mybegelight': ['#F3E8CE'],
+        'mygold': ['#AA8847'],
+        'mygray': ['#8C928E']
+      },
+    },
+  },
+}
+
